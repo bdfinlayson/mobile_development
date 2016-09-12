a3c0f3625fa4429a5ea12ab56b5e85352a12974b
Bryan Finlayson <bdfinlayson@gmail.com>
2016-09-11 21:20:46 -0500

    lecture 2 files

---
+++ lectures/Lecture02/Lecture02/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Lecture02
+//
+//  Created by Bryan Finlayson on 8/30/16.
+//  Copyright © 2016 Bryan Finlayson. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
